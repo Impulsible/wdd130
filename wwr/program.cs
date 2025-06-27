43ba2cee3ea18ce213577bfb7c78496f92ec55a0
Impulsible <henryosuagwu22@gmail.com>
2025-06-27 01:36:07 +0100

    Add files via upload

---
+++ wwr/program.cs
@@ -0,0 +1,13 @@
+using system;
+
+namespace game
+{
+    class program
+    {
+        static void main(string[] arg)
+        {
+            Console.Writeline("Hello World");
+            Console.Writeline("This is in C#");
+        }
+    }
+}
