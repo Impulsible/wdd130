8b889accbb9cfe2677905277936e34815fc6dd98
Impulsible <henryosuagwu22@gmail.com>
2025-08-09 01:16:07 +0100

    Add files via upload

--- wwr/scripts/nav.js
+++ wwr/scripts/nav.js
@@ -1,7 +1,12 @@
 // Hamburger toggle
-document.getElementById("hamburger").addEventListener("click", () => {
-  document.getElementById("navMenu").classList.toggle("show");
+const hamburger = document.getElementById('hamburger');
+const navMenu = document.getElementById('navMenu');
+
+hamburger.addEventListener('click', () => {
+  navMenu.classList.toggle('active');
+  hamburger.classList.toggle('active');
 });
+
 
 // Last Modified Date
 document.getElementById("lastModified").textContent =
